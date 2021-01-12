01c350e39defa4689459c5cf8c1bf8d71546287e
SuzukiYuki3111 <73920578+SuzukiYuki3111@users.noreply.github.com>
2021-01-12 15:33:39 +0900

    Add files via upload

---
+++ PHP_study/lottery/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>おみくじ</title>
+</head>
+<body>
+    <header>
+        2021新年運試し！
+    </header>
+    <main>
+        <form action="lottery.php">
+            <label>
+                <button type="submit" name="drawing">おみくじを引く！</button>
+            </label>
+        </form>
+    </main>
+    <footer>
+        SuzukiYuki practice
+    </footer>
+</body>
+</html>
