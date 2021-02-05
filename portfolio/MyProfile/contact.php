3d2166f21dda2c9f86be2a5c94f504156139da1d
SuzukiYuki3111 <73920578+SuzukiYuki3111@users.noreply.github.com>
2021-02-05 21:33:10 +0900

    profile
    お問い合わせフォーム作成中、LPにする

--- portfolio/MyProfile/contact.php
+++ portfolio/MyProfile/contact.php
@@ -1,3 +1,13 @@
 <?php
-    //　お問い合わせフォーム、ログイン機能を作成する
-    
+    // メールを送信できるようにする
+    mb_language("japanese");
+    mb_internal_encoding("utf-8");
+    $text=$_POST['text'];
+    $mail=$_POST['mail'];
+
+    if(mb_send_mail('yukicourage3111@gmail.com','',$text,$mail)){
+        echo "メールを送信致しました。";
+    } else{
+        echo "メールを送信できませんでした。入力していない部分があります。";
+    }
+?>
